3960bf509c0df287f4acef63368508731fec856d
João Vinicius da Costa <joaoviniciusdacosta800@gmail.com>
2022-02-24 19:10:17 -0300

    s

--- model/connect.php
+++ model/connect.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$db = "db";
-$conect = mysqli_connect($hostname,$username,$password,$db) or die ("não conectou");
+$hostname = $_ENV['URL'];
+$username = $_ENV['USERDB'];
+$password = $_ENV['PASS'];
+$db = $_ENV['DB'];
+$conect = mysqli_connect($hostname,$username,$password,$db) or die ("nÃ£o conectou");
