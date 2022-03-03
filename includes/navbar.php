be397266cca2aad1097824c8eef82f4cfedfa1d7
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2022-03-02 21:29:20 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-fixed navbar-default">
 	<div class="navbar-inner">
 		<div class="navbar-intro justify-content-xl-between">
+			<button type="button" class="btn btn-burger burger-arrowed static ml-2 d-flex d-xl-none collapsed" data-toggle-mobile="sidebar" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle sidebar">
+                <span class="bars"></span>
+            </button>
 			<a class="navbar-brand text-white">
 				<i class="fa fa-leaf">
 					<span>Controle</span>
 					<span>Estoque</span>
 				</i>
-      </a>
-    </div>
-  </div>
+      		</a>
+    	</div>
+  	</div>
 </nav>
