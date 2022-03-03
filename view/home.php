98f59733f7cf10eaa83ee99b60077351f8ce826a
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2022-03-02 21:27:32 -0300

    Update home.php

--- view/home.php
+++ view/home.php
@@ -16,7 +16,7 @@ include('../model/session.php');
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600&amp;display=swap"> 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.css">
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600&display=swap">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../includes/css/ace.min.css">
     <link rel="stylesheet" type="text/css" href="../includes/css/ace-themes.min.css">
     <link rel="stylesheet" type="text/css" href="../includes/css/ace.css">
@@ -25,6 +25,7 @@ include('../model/session.php');
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    
     <style>
         .navbar-brand span {
           font-family: 'Open Sans'
@@ -34,14 +35,17 @@ include('../model/session.php');
 
 <body>
 	<div class="body-container">
-        	<?php include("../includes/navbar.php"); ?>
+        <?php include("../includes/navbar.php"); ?>
       	<div class="main-container">
-		<?php include("../includes/sidebar.php"); ?>
+			  <?php include("../includes/sidebar.php"); ?>
       	</div>
     </div>
 
   <script src="../controller/controller_home.js"></script>
   <script src="../controller/controller_sidebar.js"></script>
+  <script src="../includes/js/sidebar.js"></script>
+  <script src="../includes/bootstrap.js"></script>
+  <script src="../includes/ace.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
